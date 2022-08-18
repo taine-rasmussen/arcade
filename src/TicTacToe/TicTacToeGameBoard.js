99f0fb2ceea99cd8f0f81dc056a734ce49eb79e3
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-08-18 22:07:52 +1200

    populate gameboard state

--- src/TicTacToe/TicTacToeGameBoard.js
+++ src/TicTacToe/TicTacToeGameBoard.js
@@ -1,19 +1,34 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './TicTacToe.scss'
 
 
 const TicTacToeGameBoard = () => {
-  const [gameboard, setGameboard] = useState([])
+  const [gameboard, setGameboard] = useState()
+  const gameCells = new Array(9).fill({ value: '' })
 
-  const gameCells = new Array(9).fill({})
+  useEffect(
+    () => {
+      setGameboard([gameCells])
+    }, [])
+
+  const handleCellClick = (i) => {
+    console.log(i)
+  }
+
+  if (!gameboard) return;
 
   return (
     <div className='tictactoe_container'>
       <div className='tictactoe_game_container'>
-        {gameCells.map((cell, i) => {
+        {gameboard[0].map((cell, i) => {
+          const { value } = cell
           return (
-            <div className='tictactoe_cell' key={i}>
-
+            <div
+              className='tictactoe_cell'
+              key={i}
+              onClick={() => { handleCellClick(i) }}
+            >
+              {value}
             </div>
           )
         })}
