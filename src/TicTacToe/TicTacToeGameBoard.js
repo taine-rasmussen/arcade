7ff45f6707c47557360f6e1a1953030757eac83b
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-08-18 21:56:56 +1200

    set up gameboard

--- src/TicTacToe/TicTacToeGameBoard.js
+++ src/TicTacToe/TicTacToeGameBoard.js
@@ -5,9 +5,19 @@ import './TicTacToe.scss'
 const TicTacToeGameBoard = () => {
   const [gameboard, setGameboard] = useState([])
 
-  return (
-    <div className='TicTacToe_container'>
+  const gameCells = new Array(9).fill({})
 
+  return (
+    <div className='tictactoe_container'>
+      <div className='tictactoe_game_container'>
+        {gameCells.map((cell, i) => {
+          return (
+            <div className='tictactoe_cell' key={i}>
+
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
